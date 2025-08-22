8cb2b2ff4ece91d3ac5b7b32171f968573be220e
Tahiel Heinze <tahielheinze@hotmail.com>
2025-08-21 22:29:31 -0300

    Ejercicios-Introduccion-a-Java

--- Unidad_1-Introduccion-a-Java/HolaMundo/src/holamundo/HolaMundo.java
+++ Unidad_1-Introduccion-a-Java/HolaMundo/src/holamundo/HolaMundo.java
@@ -16,32 +16,45 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /* EJERCICIO 2
+        /*            ---- EJERCICIO 2 ----
+        
         System.out.println("Hola, Java!");
+        
         */
         
-        /* EJERCICIO 3
+        /*            ---- EJERCICIO 3 ----
+        
         String nombre = "Tahiel";
+        
         int edad = 21;
+        
         double altura = 1.75;
+        
         boolean estudiante = true;
+        
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
         System.out.println("Altura: " + altura);
         System.out.println("Es estudiante: " + estudiante);
+        
         */
         
-        /* EJERCICIO 4
+        /*            ---- EJERCICIO 4 ----
+        
         Scanner input = new Scanner(System.in);
+        
         String nombre;
         System.out.println("Ingrese su nombre:");
         nombre = input.nextLine();
+        
         int edad;
         System.out.println("Ingrese su edad:");
         edad = input.nextInt();
+        
         double altura;
         System.out.println("Ingrese su altura:");
         altura = input.nextDouble();
+        
         boolean estudiante;
         System.out.println("Es estudiante? Conteste con True o False:");
         estudiante = input.nextBoolean();
@@ -50,9 +63,11 @@ public class HolaMundo {
         System.out.println("Edad: " + edad);
         System.out.println("Altura: " + altura);
         System.out.println("Es estudiante: " + estudiante);
+        
         */
         
-        /* EJERCICIO 5 
+        /*            ---- EJERCICIO 5 ----
+        
         Scanner input = new Scanner(System.in);
         
         int Num1;
@@ -74,7 +89,8 @@ public class HolaMundo {
         System.out.println("La Division de " +Num1+ " / " +Num2+ " = " +Division);
         */
         
-        /* EJERCICIO 6 
+        /*             ---- EJERCICIO 6 ----
+        
         String nombre = "Juan Pérez";
         int edad = 30;
         String direccion = "Calle Falsa 123";
@@ -84,7 +100,8 @@ public class HolaMundo {
         System.out.println("Direccion: " + direccion + "\n");
         */
         
-        /* EJERCICIO 7 
+        /*            ---- EJERCICIO 7 ----
+        
         Analiza el siguiente código y responde: ¿Cuáles son expresiones y cuáles 
         son instrucciones? Explica la diferencia en un breve párrafo.
         
@@ -108,7 +125,39 @@ public class HolaMundo {
         
         */
         
-        /* EJERCICIO 8 */
+        /*            ---- EJERCICIO 8 ---- 
+        Scanner input = new Scanner(System.in);
+        
+        double Num1;
+        System.out.println("Ingrese el valor para el primer numero:");
+        Num1 = input.nextInt();
+        
+        double Num2;
+        System.out.println("Ingrese el valor para el segundo numero:");
+        Num2 = input.nextInt();
+        
+        double division = Num1 / Num2;
+        
+        System.out.println("El resultado de la division de: " +Num1+ " / " +Num2+ " = " +division);
+        
+        Al utilizar double en vez de int, al colocar un numero entero como por ej. 2
+        Se ve reflejado en el sistema como 2.0 ya que Java automaticamente lo convierte al formato de flotante.
+        Esto sucede ya que al utilizar double y colocar un numero entero Java lo almacena como 2.0 para que coincida con el tipo double.
+        
+        */
+        
+        /*            ---- EJERCICIO 9 ----
+        
+            public class ErrorEjemplo {
+            public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Ingresa tu nombre: ");
+            String nombre = scanner.nextInt(); // ERROR
+            System.out.println("Hola, " + nombre);
+            }
+        }
+        */
+        
         
         
 }
