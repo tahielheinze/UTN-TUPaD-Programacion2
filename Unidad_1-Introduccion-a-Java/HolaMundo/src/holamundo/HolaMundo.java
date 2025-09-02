584bbd5e3c81ca02636877b40bbcbb68cb1cc518
Tahiel Heinze <tahielheinze@hotmail.com>
2025-09-02 20:41:52 -0300

    Ejercicios-Introduccion-a-Java

--- Unidad_1-Introduccion-a-Java/HolaMundo/src/holamundo/HolaMundo.java
+++ Unidad_1-Introduccion-a-Java/HolaMundo/src/holamundo/HolaMundo.java
@@ -16,88 +16,88 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*            ---- EJERCICIO 2 ----
+        /*            ---- EJERCICIO 2 ---- */
         
-        System.out.println("Hola, Java!");
-        
+            System.out.println("Hola, Java!");
+
+            
+
+            /*            ---- EJERCICIO 3 ---- 
+
+            String nombre = "Tahiel";
+
+            int edad = 21;
+
+            double altura = 1.75;
+
+            boolean estudiante = true;
+
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Edad: " + edad);
+            System.out.println("Altura: " + altura);
+            System.out.println("Es estudiante: " + estudiante);
         */
         
-        /*            ---- EJERCICIO 3 ----
         
-        String nombre = "Tahiel";
+        /*            ---- EJERCICIO 4 ---- 
         
-        int edad = 21;
-        
-        double altura = 1.75;
-        
-        boolean estudiante = true;
-        
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Altura: " + altura);
-        System.out.println("Es estudiante: " + estudiante);
-        
+            Scanner input = new Scanner(System.in);
+
+            String nombre;
+            System.out.println("Ingrese su nombre:");
+            nombre = input.nextLine();
+
+            int edad;
+            System.out.println("Ingrese su edad:");
+            edad = input.nextInt();
+
+            double altura;
+            System.out.println("Ingrese su altura:");
+            altura = input.nextDouble();
+
+            boolean estudiante;
+            System.out.println("Es estudiante? Conteste con True o False:");
+            estudiante = input.nextBoolean();
+
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Edad: " + edad);
+            System.out.println("Altura: " + altura);
+            System.out.println("Es estudiante: " + estudiante);
         */
         
-        /*            ---- EJERCICIO 4 ----
         
-        Scanner input = new Scanner(System.in);
+        /*            ---- EJERCICIO 5 ---- 
         
-        String nombre;
-        System.out.println("Ingrese su nombre:");
-        nombre = input.nextLine();
-        
-        int edad;
-        System.out.println("Ingrese su edad:");
-        edad = input.nextInt();
-        
-        double altura;
-        System.out.println("Ingrese su altura:");
-        altura = input.nextDouble();
-        
-        boolean estudiante;
-        System.out.println("Es estudiante? Conteste con True o False:");
-        estudiante = input.nextBoolean();
-        
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Altura: " + altura);
-        System.out.println("Es estudiante: " + estudiante);
-        
+            Scanner input = new Scanner(System.in);
+
+            int Num1;
+            System.out.println("Ingrese el valor del primer numero: ");
+            Num1 = input.nextInt();
+
+            int Num2;
+            System.out.println("Ingrese el valor del segundo numero: ");
+            Num2 = input.nextInt();
+
+            int Suma = Num1 + Num2;
+            int Multiplicacion = Num1 * Num2;
+            int Resta = Num1 - Num2;
+            int Division = Num1 / Num2;
+
+            System.out.println("La suma de " +Num1+ " + " +Num2+ " = " +Suma);
+            System.out.println("La Multiplicacion de " +Num1+ " x " +Num2+ " = " +Multiplicacion);
+            System.out.println("La Resta de " +Num1+ " - " +Num2+ " = " +Resta);
+            System.out.println("La Division de " +Num1+ " / " +Num2+ " = " +Division);
         */
         
-        /*            ---- EJERCICIO 5 ----
+        /*             ---- EJERCICIO 6 ---- 
         
-        Scanner input = new Scanner(System.in);
-        
-        int Num1;
-        System.out.println("Ingrese el valor del primer numero:");
-        Num1 = input.nextInt();
-        
-        int Num2;
-        System.out.println("Ingrese el valor del segundo numero:");
-        Num2 = input.nextInt();
-        
-        int Suma = Num1 + Num2;
-        int Multiplicacion = Num1 * Num2;
-        int Resta = Num1 - Num2;
-        int Division = Num1 / Num2;
-        
-        System.out.println("La suma de " +Num1+ " + " +Num2+ " = " +Suma);
-        System.out.println("La Multiplicacion de " +Num1+ " x " +Num2+ " = " +Multiplicacion);
-        System.out.println("La Resta de " +Num1+ " - " +Num2+ " = " +Resta);
-        System.out.println("La Division de " +Num1+ " / " +Num2+ " = " +Division);
-        */
-        
-        /*             ---- EJERCICIO 6 ----
-        
-        String nombre = "Juan Pérez";
-        int edad = 30;
-        String direccion = "Calle Falsa 123";
-        
-        System.out.println("Nombre: " + nombre + "\n");
-        System.out.println("Edad: " + edad + "\n");
-        System.out.println("Direccion: " + direccion + "\n");
+            String nombre = "Juan Pérez";
+            int edad = 30;
+            String direccion = "Calle Falsa 123";
+
+            System.out.println("Nombre: " + nombre + "\n");
+            System.out.println("Edad: " + edad + "\n");
+            System.out.println("Direccion: " + direccion + "\n");
         */
         
         /*            ---- EJERCICIO 7 ----
@@ -120,43 +120,76 @@ public class HolaMundo {
         Instrucciones: Llamada a metodo
         Expresiones: System.out (acceso a campo), x (variable)
         
-        Expresiones = Es la combinacion de valores, variables, operados y metodos para producir un valor unico. La expresion por si sola no realiza una accion
+        Expresiones = Es la combinacion de valores, variables, operados y metodos para producir un valor unico. La expresion por si sola no realiza una accion.
         Instruccion = Es una linea de codigo que indica una accion, siempre debe de terminar con un ;. Una instruccion contiene una o más expresiones.
         
         */
         
-        /*            ---- EJERCICIO 8 ---- 
-        Scanner input = new Scanner(System.in);
+        /*            ---- EJERCICIO 8 ----
+            Scanner input = new Scanner(System.in);
+
+            int Num1;
+            System.out.println("Ingrese el valor para el primer numero:");
+            Num1 = input.nextInt();
+
+            int Num2;
+            System.out.println("Ingrese el valor para el segundo numero:");
+            Num2 = input.nextInt();
+
+            int division = Num1 / Num2;
+
+            System.out.println("El resultado de la division de: " +Num1+ " / " +Num2+ " = " +division);
         
-        double Num1;
-        System.out.println("Ingrese el valor para el primer numero:");
-        Num1 = input.nextInt();
-        
-        double Num2;
-        System.out.println("Ingrese el valor para el segundo numero:");
-        Num2 = input.nextInt();
-        
-        double division = Num1 / Num2;
-        
-        System.out.println("El resultado de la division de: " +Num1+ " / " +Num2+ " = " +division);
-        
-        Al utilizar double en vez de int, al colocar un numero entero como por ej. 2
+         Al utilizar double en vez de int, al colocar un numero entero como por ej. 2
         Se ve reflejado en el sistema como 2.0 ya que Java automaticamente lo convierte al formato de flotante.
-        Esto sucede ya que al utilizar double y colocar un numero entero Java lo almacena como 2.0 para que coincida con el tipo double.
+        Esto sucede ya que al utilizar double y colocar un numero entero Java lo almacena como 2.0 para que coincida con el tipo double. 
         
         */
         
         /*            ---- EJERCICIO 9 ----
         
             public class ErrorEjemplo {
-            public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Ingresa tu nombre: ");
-            String nombre = scanner.nextInt(); // ERROR
-            System.out.println("Hola, " + nombre);
+                public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Ingresa tu nombre: ");
+                String nombre = scanner.nextInt(); // ERROR
+                System.out.println("Hola, " + nombre);
+                }
             }
-        }
+            
+        El ERROR que contiene el codigo es en la seccion: String nombre = scanner.nextInt(); La cual la variable Nombre deberia de leer una cadena de texto (string),
+        pero en el código esta diseñado para leer números enteros. Al correr el programa y el usuario colocar un Nombre como se espera que haga,
+        el compilador arrojaria un mensaje de error "InputMismatchException", esto es pq el programa esta preparado para leer unicamente números enteros, no string o cadena de textos.
+            
+            Para arreglar el error haría lo siguiente:
+        
+            public class ErrorEjemplo {
+                public static void main (String[] args) {
+                Scanner scanner = new Scanner (System.in);
+                System.out.print ("Ingresa tu nombre: ");
+                String nombre = scanner.nextLine();
+                System.out.println("Hola, " + nombre);
+                }
+            }
         */
+        
+        /*            ---- EJERCICIO 10 ---- 
+        
+            public class PruebaEscritorio {
+                public static void main(String[] args) {
+                int a = 5;
+                int b = 2;
+                int resultado = a / b;
+                System.out.println("Resultado: " + resultado);
+                }
+            }
+        
+        El programa al realizar la division de 5 / 2 arroja el resultado de 2. Esto se debe a que Java esta realizando la division entera entre esos 2 números enteros (int).
+        Esto significa que los decimales de la division se truncan y a su vez Java no redondea para arriba el número
+        
+        */
+        
+
         
         
         
